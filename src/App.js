2041c083bac2753afd88767ccee76b2cef8c8b01
jinho1011 <gino9940@gmail.com>
2019-03-17 16:19:47 +0900

    get location done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css'
+import React, { Component } from "react";
+import "./App.css";
+import Location from "./location";
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    this.getGeolocation()
+  }
+
+  getGeolocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          this.getPosition(position.coords.latitude, position.coords.longitude);
+        },
+        error => {
+          console.error(error);
+        },
+        {
+          enableHighAccuracy: false,
+          maximumAge: 0,
+          timeout: Infinity
+        }
+      );
+    }
+  };
+
+  getPosition = async (lat, lng) => {
+    const FETCH_URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyCZVEs9_xdPkB7tK1buJKkWjZo_Sqy5MbE`
+    var city = await fetch(FETCH_URL)
+      .then(res => res.json())
+      .catch(err => console.log('TCL: App -> getPosition -> err', err))
+    //8XX9+HQ 대한민국 경기도 의왕시
+    city = city.plus_code.compound_code.split(' ');
+    city = city[city.length-1]
+		console.log('TCL: App -> getPosition -> city', city)
+    return city
+  };
+
   render() {
+    // const { dust } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      // <div className={dust ? "App" : "App--loading"}>
+      //   {dust ? this.renderDust() : <Location />}
+      // </div>
+      <div>
+        <Location />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/location.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./location.css";
+
+function Location() {
+  return (
+    <div>
+        <h1></h1>
+    </div>
+  );
+}
+
+export default Location;
