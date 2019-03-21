12850bb1607b5b9d5a6039eec04ae09bce7efb1f
전진호 <gino9940@gmail.com>
2019-03-21 10:55:26 +0900

    getting TM coord done

--- src/App.js
+++ src/App.js
@@ -6,55 +6,33 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    var coord = this.getGeolocation()
-    console.log('TCL: App -> componentDidMount -> coord', coord)
-    
-    this.getTMcoord(coord)
+    navigator.geolocation.getCurrentPosition(position => {
+      this.getTMcoord(position)
+    });
   }
 
-  getGeolocation = () => {
-    var coord = {}
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          coord.lat = position.coords.latitude
-          coord.long = position.coords.longitude
-        },
-        error => {
-          console.error(error);
-        },
-        {
-          enableHighAccuracy: false,
-          maximumAge: 0,
-          timeout: Infinity
-        }
-      );
-    }
-    
-		console.log('TCL: App -> getGeolocation -> coord', coord)
-    return coord
+  getTMcoord = async (position) => {
+    const coord = await position
+    const lat = coord.coords.latitude
+    const lng = coord.coords.longitude
+
+    var API_URL = `https://dapi.kakao.com/v2/local/geo/transcoord.json?x=${lat}&y=${lng}&input_coord=WTM&output_coord=TM`
+    console.log('TCL: App -> getTMcoord -> API_URL', API_URL)
+    var API_KEY = 'KakaoAK c981c546d4e1806ac95ac294b40fc615'
+    var res = fetch(API_URL, {
+      "headers": {
+        'Authorization': API_KEY
+      }
+    })
+      .then(res => res.json())
+      .then(res => res.documents[0])
+
+    res = Promise.resolve(res)
+    .then(res => {
+      
+    })
+
   };
-
-  getTMcoord = async (coord) => {
-    console.log('TCL: App -> getTMcoord -> coord', coord)
-    var lat = await coord.lat
-		console.log('TCL: App -> getTMcoord -> lat', lat)
-    
-  }
-
-  // KaKao API를 이용하면 되서 필요가 없어졌음ㅋ
-
-  // getPosition = async (lat, lng) => {
-  //   const FETCH_URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyCZVEs9_xdPkB7tK1buJKkWjZo_Sqy5MbE`
-  //   var city = await fetch(FETCH_URL)
-  //     .then(res => res.json())
-  //     .catch(err => console.log('TCL: App -> getPosition -> err', err))
-  //   //8XX9+HQ 대한민국 경기도 의왕시
-  //   city = city.plus_code.compound_code.split(' ');
-  //   city = city[city.length-1]
-  // 	console.log('TCL: App -> getPosition -> city', city)
-  //   return city
-  // };
 
   render() {
     // const { dust } = this.state;
