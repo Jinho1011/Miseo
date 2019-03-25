3c2784f2a6417e03161659681f4ac2d5246bc6ce
전진호 <gino9940@gmail.com>
2019-03-25 12:39:28 +0900

    getting dong name dine

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 import Location from "./location";
-import { disconnect } from "cluster";
 
 const KAKAO_API_KEY = "KakaoAK c981c546d4e1806ac95ac294b40fc615";
 
@@ -13,20 +12,30 @@ class App extends Component {
   }
 
   getDistrict = () => {
+    var district
     navigator.geolocation.getCurrentPosition(position => {
-      var coords = position.coords;
-      const API_URL = `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${coords.longitude}&y=${coords.latitude}`;
-      fetch(API_URL, {
-        headers: {
-          Authorization: KAKAO_API_KEY
-        }
-      })
-        .then(res => res.json())
+      const coords = position.coords;
+      const district = this.callDistrcitApi(coords)
+      Promise.resolve(district)
+        .then(res => res)
         .then(res => {
-          var tmp = res.documents[0].address_name
-        });
+          res = res.split(' ')
+          var dong = res[res.length-1]
+          // Getting 동 이름 done
+        })
     });
   };
+
+  callDistrcitApi = (coords) => {
+    const API_URL = `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${coords.longitude}&y=${coords.latitude}`;
+    return fetch(API_URL, {
+      headers: {
+        "Authorization": KAKAO_API_KEY
+      }
+    })
+      .then(res => res.json())
+      .then(obj => obj.documents[0].address_name)
+  }
 
   render() {
     return (
